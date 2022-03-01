b4ce588950df956cd7166a38e0bb4fc6ae50a69a
Paleksz <96802620+Hardyzom@users.noreply.github.com>
2022-03-01 16:24:46 +0100

    Add files via upload

---
+++ mariov3.c
@@ -0,0 +1,29 @@
+#include stdio.h
+
+int main()
+{
+    int a;
+    int b=1;
+    int c=1;
+    printf(Adj meg egy magasságot);
+    scanf(%d, &a);
+    int d;
+    d=a;
+    for(int i=1;i=a;i++){
+        for(int i=1;id;i++){
+            printf( );
+        }
+        d--;
+        for(int i=1;i=b;i++){
+            printf(#);
+        }
+        b++;
+        printf(  );
+        for(int i=1;i=c;i++){
+            printf(#);
+        }
+        c++;
+        printf(n);
+    }   
+    return 0;
+}
